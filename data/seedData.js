c0b29402c6b181168213df4ec660b6859d1e6419
PR2DEV <sbousnina@pr2.ca>
2015-02-18 12:52:23 -0500

    v 0.0.1
    test

---
+++ data/seedData.js
@@ -0,0 +1,16 @@
+﻿(function (seedData) 
+{
+    seedData.initialNotes = 
+    [
+        { name: "M",
+          notes: "MangoDB"}, 
+        { name: "E",
+          notes: "Express"},
+        { name: "A",
+          notes: "Angular"
+        },
+        { name: "N",
+          notes: "Node"}
+    ];
+
+})(module.exports);
