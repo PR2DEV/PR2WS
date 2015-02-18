f3a72bd1a3063530c6b4d0cc7d4a89022a148d08
PR2DEV <sbousnina@pr2.ca>
2015-02-18 13:11:15 -0500

    v 0.0.1
    1ere solution clean

---
+++ server.js
@@ -0,0 +1,17 @@
+var http = require("http");
+var express = require("express");
+var app = express();
+
+var controllers = require("./controllers");
+
+// Setup the View Engine
+app.set("view engine", "vash");
+
+// set the public static resource folder
+app.use(express.static(__dirname + "/public"));
+
+// Map the routes
+controllers.init(app);
+
+var server = http.createServer(app);
+server.listen(3000);
